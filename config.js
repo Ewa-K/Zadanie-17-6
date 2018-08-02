0814dc89841476658042331ab6ee81f496981494
Ewa.K <ewa_1988@yahoo.com>
2018-08-02 14:56:45 +0200

    Init app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '975361138437-ptp31a0a6213gfk71nupn9uo82cmsig1.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'Ig12fOubpK6_f5oBwDODGu4g',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
